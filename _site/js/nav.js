9159026ae0b9717ce137d45e6e3a21525008f983
Seabagel <hsrt1367tim@gmail.com>
2022-09-07 15:07:36 -0700

    flush

--- _site/js/nav.js
+++
@@ -1,6 +0,0 @@
-function toggleElement(elementID) {
-  var x = document.getElementById(elementID);
-  x.style.display == "none"
-    ? (x.style.display = "block")
-    : (x.style.display = "none");
-}
